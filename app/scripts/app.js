804ab3d81760c9f126888fa052d33afd17e20f02
Kirill_Malets <kirill_malets@epam.com>
2014-07-24 11:05:29 +0300

    One more commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTestAppApp
+ * @description
+ * # angularTestAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTestAppApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+]);
